348fdd137994ffaddd330789cebbf195b0486e85
jojohorak <jehorak@utexas.edu>
2021-09-26 12:22:31 -0500

    title screen and spritesheet

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public TextMeshProUGUI title, start, close;
+    public float fadeOutLength = 1f;
+    
+    public void StartPressed()
+    {
+        Color white = new Color(0, 0, 0, 0);
+        title.DOColor(white, fadeOutLength);
+        start.DOColor(white, fadeOutLength);
+        close.DOColor(white, fadeOutLength);
+    }
+
+    public void ClosePressed()
+    {
+        Application.Quit();
+    }
+
+}
